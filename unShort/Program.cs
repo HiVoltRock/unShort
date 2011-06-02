82a16c166da46a3312621b50f49abe468c1f0d83
Cory M <HiVoltList@gmail.com>
2011-06-02 16:59:01 -0400

    Returns unshortened URL in console

--- unShort/Program.cs
+++ unShort/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
 
 namespace unShort
 {
@@ -9,12 +12,20 @@ namespace unShort
     {
         static void Main(string[] args)
         {
-            Console.Writeln("Please enter the link to be unshortened: ");
+            Console.WriteLine("Please enter the link to be unshortened: ");
+            string link = Console.ReadLine();
+            unShorten(link);
         }
 
         static void unShorten(String link)
         {
+            //makes an http request and looks at the response as the unshortened URL (URI)
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            String url = request.Address.ToString();
 
+            Console.WriteLine("URL is: \n" + url);
+            Console.ReadLine();
         }
     }
 
